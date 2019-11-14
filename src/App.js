f118ddabaccd12c6255d9a3dd4a655b4e0bea048
Spencer Lee <spencerlee96@gmail.com>
2019-11-14 15:59:06 -0700

    react day 3 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './Components/Data'
+import DataComponent from './Components/DataComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        people: data,
+        i: 0
+    }
+  }
+
+  handleClick = (e) => {
+    if(e.target.name === 'next'){
+        this.setState({
+            i: this.state.i + 1
+        })
+    }   else {
+        this.setState({
+            i: this.state.i - 1
+        })
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          Home
+        </header>
+        <div className='App-main'>
+          {<DataComponent person={this.state.people[this.state.i]} />}
+          <div className='Button-div'>
+            <button className='App-button' name="next" onClick={this.handleClick}> Next &#62; </button>
+            <span className='counter'> {this.state.i + 1}/25 </span>
+            <button className='App-button' name="previous" onClick={this.handleClick}> &#60; Previous</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/DataComponent.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import data from './Data'
+
+class DataComponent extends Component {
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        let person = this.props.person
+        
+        return(
+            <div className='Display-component'>
+                <div className='Display-subdiv'>
+                    <h2 className='name'>{person.name.first} {person.name.last}</h2>
+                    <h3>From: {person.city}, {person.country}</h3>
+                    <h3>Job Title: {person.title}</h3>
+                    <h3 className='employer'>Employer: {person.employer}</h3>
+                    <h3>Favorite Movies:
+                        <ol className='list'>
+                            <li>{person.favoriteMovies[0]}</li>
+                            <li>{person.favoriteMovies[1]}</li>
+                            <li>{person.favoriteMovies[2]}</li>
+                        </ol>
+                    </h3>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DataComponent;
